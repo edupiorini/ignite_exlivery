01ea84ce959846a09e54b1a30c501255bcf1a9b0
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-19 05:54:58 -0300

    item now has validations

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -8,6 +8,14 @@ defmodule Exlivery.Orders.Item do
 
   def build(description, category, unit_price, quantity)
       when quantity > 0 and category in @categories do
+    unit_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+  end
+
+  def build(_description, _category, _unit_price, _quantity), do: {:error, "Invalid parameters"}
+
+  defp build_item({:ok, unit_price}, description, category, quantity) do
     {:ok,
      %__MODULE__{
        description: description,
@@ -16,4 +24,6 @@ defmodule Exlivery.Orders.Item do
        quantity: quantity
      }}
   end
+
+  defp build_item(:error, _description, _category, _quantity), do: {:error, "Invalid price"}
 end
