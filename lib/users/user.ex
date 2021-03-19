17a1d32d28acf6a02abd0b5a9f805c9d975fdb27
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-19 06:13:26 -0300

    adress param was missing

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,12 +1,13 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age, :adress]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+  def build(name, email, cpf, age, adress) when age >= 18 and is_bitstring(cpf) do
     {:ok,
      %__MODULE__{
+       adress: adress,
        name: name,
        email: email,
        cpf: cpf,
