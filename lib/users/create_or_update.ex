d26c9dd0bdeb7d6953c2407c29732814bb5d6bd8
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-22 07:29:46 -0300

    module created and set

---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,13 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
+
+  def call(%{name: name, adress: adress, email: email, cpf: cpf, age: age}) do
+    adress
+    |> User.build(name, email, cpf, age)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:error, _reason} = error), do: error
+end
