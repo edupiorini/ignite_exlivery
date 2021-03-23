706184aa550a7c6cd2b8d6fd023c53ceafe694cd
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-23 06:03:47 -0300

    functions set to create/update orders

--- lib/orders/create_or_update.ex
+++ lib/orders/create_or_update.ex
@@ -1,5 +1,7 @@
 defmodule Exlivery.Orders.CreateOrUpdate do
   alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.{Item, Order}
+
   alias Exlivery.Users.Agent, as: UserAgent
 
   def call(%{user_cpf: user_cpf, items: items}) do
@@ -10,5 +12,27 @@ defmodule Exlivery.Orders.CreateOrUpdate do
     else
       error -> error
     end
+  end
+
+  defp build_items(items) do
+    items
+    |> Enum.map(&build_item/1)
+    |> handle_build()
+  end
+
+  defp build_item(%{
+         description: description,
+         ategory: category,
+         unit_price: unit_price,
+         quantity: quantity
+       }) do
+    case Item.build(description, category, unit_price, quantity) do
+      {:ok, item} -> item
+      {:error, _reason} = error -> error
+    end
+  end
+
+  defp handle_build(items) do
+    if Enum.all?(items, &is_struct/1), do: {:ok, items}, else: {:error, "Invalid items"}
   end
 end
